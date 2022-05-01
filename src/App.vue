513c97f418b9f88bbe49e76f8d7cdbef98f9ed7a
mccrush <mccrush2027@gmail.com>
2022-05-01 16:01:40 +0700

    Add colors

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="text-success">
-    <h2>Hello!</h2>
+  <div class="height-100vh d-flex align-items-center justify-content-center">
+    <div class="bg-yellow shadow rounded-3 p-3">
+      <div class="row border ps-2 pe-2">Top</div>
+      <div class="row border ps-2 pe-2">Bottom</div>
+    </div>
   </div>
 </template>
 
@@ -10,16 +13,40 @@ export default {}
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
 .color-boloto {
   color: #206a5d;
 }
+
 .color-green {
   color: #81b214;
 }
+
 .color-yellow {
   color: #ffcc29;
 }
+
 .color-orange {
   color: #f58634;
+}
+
+.bg-boloto {
+  background: #206a5d;
+}
+.bg-green {
+  background: #81b214;
+}
+.bg-yellow {
+  background: #ffcc29;
+}
+.bg-orange {
+  background: #f58634;
+}
+
+.height-100vh {
+  height: 100vh;
 }
 </style>
